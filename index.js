7d7b289a90a1aca3d1f5a18442bdcaabfc82b1d5
merry-lab-3791 <emilyroulund@gmail.com>
2019-03-19 01:56:21 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,8 +7,8 @@ function subtract (a, b){
 function multiply (a, b){ 
   return a * b ; 
 }
-function add (a, b){ 
-  return (a / b) ; 
+function divide (a, b){ 
+  return a / b ; 
 }
 function inc(n){
   n++;
@@ -19,5 +19,10 @@ function dec(n){
   return n;
 }
 function makeInt(n){
+  
+}
+function makeInt(n){
+  
+}function makeInt(n){
   
 }
